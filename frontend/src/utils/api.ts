5bff0c5c232fff25cd014cc7aaed9bd50a59e040
Paskalis Abhista Bagaskara Y <paskalis.abhista@ui.ac.id>
2023-05-23 14:55:59 +0700

    commit ulang

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,5 +1,5 @@
-export const AUTH_BACKEND_URL = "http://localhost:8000";      // Local
-// export const AUTH_BACKEND_URL = "http://34.122.28.227/api/orchestrator";    // Deployment
+// export const AUTH_BACKEND_URL = "http://localhost:8000";      // Local
+export const AUTH_BACKEND_URL = "http://34.122.28.227/api/orchestrator";    // Deployment
 
 // export const MENU_URL = "http://menu-service:8001/menu";    // Local
 export const MENU_URL = "http://34.122.28.227/api/menu";    // Deployment
