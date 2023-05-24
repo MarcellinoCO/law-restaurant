5eed786b4a28c50d7e97f9ff91b54ce1f60db0db
Ahmad Rafi Wirana <rafi.wirana@rey.id>
2023-05-24 20:56:02 +0700

    chore: change class to classname

--- frontend/src/containers/DashboardAdmin/index.jsx
+++ frontend/src/containers/DashboardAdmin/index.jsx
@@ -97,13 +97,13 @@ export default function DashboardAdminContainer() {
                                 Menu Management
                             </h1>
                             <section>
-                                <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-                                    <h2 class="mb-4 text-xl font-bold text-gray-900">
+                                <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                                    <h2 className="mb-4 text-xl font-bold text-gray-900">
                                         Add a New Menu
                                     </h2>
                                     <form onSubmit={handleCreateMenu}>
-                                        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                                            <div class="sm:col-span-2">
+                                        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                            <div className="sm:col-span-2">
                                                 <label
                                                     htmlFor="name"
                                                     className="block mb-2 text-sm font-medium text-gray-900 "
@@ -120,12 +120,12 @@ export default function DashboardAdminContainer() {
                                                             e.target.value
                                                         )
                                                     }
-                                                    className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 dark:text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                                     placeholder="Type Menu Name"
                                                     required
                                                 />
                                             </div>
-                                            <div class="w-full">
+                                            <div className="w-full">
                                                 <label
                                                     htmlFor="description"
                                                     className="block mb-2 text-sm font-medium text-gray-900"
@@ -146,9 +146,9 @@ export default function DashboardAdminContainer() {
                                                     placeholder="Menu Description"
                                                 />
                                             </div>
-                                            <div class="w-full">
+                                            <div className="w-full">
                                                 <label
-                                                    for="price"
+                                                    htmlFor="price"
                                                     className="block mb-2 text-sm font-medium text-gray-900"
                                                 >
                                                     Price
@@ -163,14 +163,14 @@ export default function DashboardAdminContainer() {
                                                             e.target.value
                                                         )
                                                     }
-                                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                                     placeholder="20000"
                                                     required
                                                 />
                                             </div>
                                             <div>
                                                 <label
-                                                    for="category"
+                                                    htmlFor="category"
                                                     className="block mb-2 text-sm font-medium text-gray-900"
                                                 >
                                                     Category
@@ -241,10 +241,10 @@ export default function DashboardAdminContainer() {
                                 Coupon Management
                             </h1>
                             <section>
-                                <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+                                <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
                                     <form onSubmit={() => {}}>
-                                        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                                            <div class="sm:col-span-2">
+                                        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                                            <div className="sm:col-span-2">
                                                 <label
                                                     htmlFor="coupon"
                                                     className="block mb-2 text-sm font-medium text-gray-900 "
